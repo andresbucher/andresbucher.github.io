033f7c05f38f0accaf44b6a5eabdce08715ce7c9
Andrés Bucher <andres99@gmx.net>
2022-12-03 17:49:07 +0100

    make if work lmao

--- public/js/manderindli.js
+++ public/js/manderindli.js
@@ -1,15 +1,14 @@
 import { async } from "@firebase/util";
 import { getSlicesFromFirebase, addNewSliceToFirebase } from "./firebase.js";
+import Plotly from "../../node_modules/plotly.js-dist";
+import { doc } from "firebase/firestore/lite";
 
-const labelarray = [6,7,8,9,10,11,12,13,14,15];
-var xx = 0;
-var yy = 0;
 
 export async function add(amount) {
-    if (amount > 0) {
+    if (amount > 0 && amount < 16) {
         await addNewSliceToFirebase(amount);
     } else {
-        console.log("invalid amount " + amount);
+        alert("invalid amount suspected: " + amount + " please send a picture of the tangerine to Andi for data-manipulation inspection");
     }
 };
 
@@ -42,91 +41,84 @@ export async function getMean() {
     const amounts = data.map((slice) => slice.amount);
     const sum = amounts.reduce((a, b) => a + b, 0);
     const mean = sum / amounts.length;
-    return mean;
+    let rounded_mean = mean.toFixed(2)
+    return rounded_mean;
 };
 
 export async function getStabOfSlices() {
     const data = await getSlicesFromFirebase();
     const amounts = data.map((slice) => slice.amount);
-    const stab = amounts.reduce((a, b) => a + b, 0);
-    return stab;
+    const n = amounts.length;
+    const mean = amounts.reduce((a, b) => a + b) / n;
+    var stab = Math.sqrt(amounts.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+    let rounded_stab = stab.toFixed(2);
+    return rounded_stab;
 };
 
 export async function count_bins_for_barplot() {
+    const labelarray = [6,7,8,9,10,11,12,13,14,15];
+    var xx = 0;
+    var yy = 0;
     const ray0 = labelarray;
     const ray1 = await getSlicesFromFirebase();
-    let bar_plot_array = [2,1];
-    const zz = await getSumOfFruits();
+    const amounts = ray1.map((slice) => slice.amount);
+    var bar_plot_array = [];
+    const zz = parseInt(await getSumOfFruits());
+
     for (var i = 0; i < ray0.length; i++){
-        for (var j = 0; j < ray1.length; j++){
-            if (ray0[i] == ray1[j]) {
+        for (var j = 0; j < amounts.length; j++){
+            if (ray0[i] == amounts[j]) {
                 xx += 1;
-            }
-        }
-        yy = xx / +(zz);
-        console.log(yy);
+            };
+        };
+        yy = parseFloat(xx / zz);
         bar_plot_array.push(yy);
-    }
-    console.log(bar_plot_array);
+        xx = 0;
+    };
     return bar_plot_array;
 };
 
-export async function draw_boxplot() {
-    ray = await getSlicesFromFirebase();
-    var boxplot = {
-    x: "boxplot",
-    y: ray,
-    name: "slices",
-    marker: {color: '#FF851B'},
-    type: "box"
-    };  
-    
-    var layout = {
-        yaxis: {
-            title: "Anzahl Schnitze", 
-            zeroline: false
-        },
-    };
-    const boxplotid = document.getElementById("plot-02-boxplot").innerHTML;
-    Plotly.newPlot('plot-02-boxplot', boxplotid, boxplot);
-};
-
-/* QQQQQQQQQQQQQQQQQQQQ */
-
-
-function draw_bar_diagram(){
-    ray0 = labelarray;
-    ray1 = state["bar_plot_array"];
-    //console.log(state["bar_plot_array"])
-    var data = [
-        {
+export async function draw_bar_diagram() {
+    const labelarray = [6,7,8,9,10,11,12,13,14,15];
+    var ray0 = labelarray;
+    var ray1 = await count_bins_for_barplot();
+    console.log(ray1, "RAY1");
+    var data = [{
             x: ray0,
             y: ray1,
-            type: 'bar'
+            marker: {color: '#FF851B'},
+            type: 'bar',
         }
     ];
+
     var layout = {
         xaxis: {
             showticklabels: true,
             type: 'category',
         }
-    }
-    Plotly.newPlot('plot-01-bar', data, layout);
-}
+    };
+    var config = {responsive: true}
+    Plotly.newPlot("plot-01-bar", data, layout, config);
+};
 
-function update_stats() {
-    sum();
-    median();
-    mean();
-    stab();
-    totalsli();
-    count_bins_for_barplot();
-}
-
-function draw_stats(){
-    document.getElementById('sum_of_fruits').innerHTML = state["sum"];
-    document.getElementById("total_slices").innerHTML = state["totalsli"];
-    document.getElementById('median_of_slices').innerHTML = state["median"];
-    document.getElementById('mean_of_slices').innerHTML = state["mean"];
-    document.getElementById('stab_of_slices').innerHTML =  state["stdv"];
-}
+export async function draw_boxplot() {
+    var ray = await getSlicesFromFirebase();
+    const amounts = ray.map((slice) => slice.amount);
+    var data = [{
+    x: "boxplot",
+    y: amounts,
+    name: "segments",
+    marker: {color: '#FF851B'},
+    type: "box"
+        }
+    ];  
+    
+    var layout = {
+        yaxis: {
+            title: "Anzahl Segmente", 
+            zeroline: false
+        },
+    };
+    var config = {responsive: true}
+    Plotly.newPlot('plot-02-boxplot', data, layout, config);
+};
